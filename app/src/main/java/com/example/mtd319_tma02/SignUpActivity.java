10af291c00a9742144b724ed13892d4199a32171
zqpang001 <pzq960@gmail.com>
2022-10-26 08:16:26 +0800

    v1.5 Sign Up validate fields completed.

--- app/src/main/java/com/example/mtd319_tma02/SignUpActivity.java
+++ app/src/main/java/com/example/mtd319_tma02/SignUpActivity.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.util.JsonReader;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -24,10 +26,17 @@ import java.util.Map;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    TextView passwordMsg;
+    TextView matchPasswordMsg;
+    TextView usernameMsg;
+    TextView emailMsg;
+    TextView agreeMsg;
+    CheckBox agreeCheckBox;
     EditText userNameEditText;
+    EditText emailEditText;
     EditText passwordEditText;
-
-
+    EditText cPasswordEditText;
+    Intent intent = new Intent();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,34 +44,74 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         userNameEditText = findViewById(R.id.userName_SignUp_TxtField);
+        emailEditText = findViewById(R.id.email_SignUp_TxtField);
         passwordEditText = findViewById(R.id.password_SignUp_TxtField);
-
+        cPasswordEditText = findViewById(R.id.cPassword_SignUp_TxtField);
+        agreeCheckBox = findViewById(R.id.agree_CheckBox);
+        usernameMsg = findViewById(R.id.usernameMsg);
+        emailMsg = findViewById(R.id.emailMsg);
+        passwordMsg = findViewById(R.id.passwordMsg);
+        matchPasswordMsg = findViewById(R.id.matchPasswordMsg);
+        agreeMsg = findViewById(R.id.agreeMsg);
     }
 
     public void signIn(View view) {
-        Intent intent = new Intent();
         intent = new Intent(this,SignInActivity.class);
         startActivity(intent);
     }
 
     public void clickSignUpBtn(View view) {
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://mtd319-ed05.restdb.io/rest/credential?apikey=6357f014626b9c747864aeeb";
-        StringRequest stringRequest = new StringRequest(Request.Method.POST,url,
-                response -> Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show(),
-                error -> Toast.makeText(this,"Error",Toast.LENGTH_SHORT).show()) {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Credential credential = new Credential(userNameEditText.getText().toString(),passwordEditText.getText().toString());
-//                Map<String, String> params = new HashMap<>();
-//                params.put("dasdadsa" ,"sadsada");
-                Gson gson = new Gson();
-                String jsonString = gson.toJson(credential);
-                Map map = gson.fromJson(jsonString, Map.class);
-                Log.d("getMap: ",map.toString());
-                return map;
+        if(userNameEditText.getText().toString().isEmpty()){
+            usernameMsg.setVisibility(View.VISIBLE);
+        }else{
+            usernameMsg.setVisibility(View.INVISIBLE);
+        }
+
+        if(emailEditText.getText().toString().isEmpty()){
+            emailMsg.setVisibility(View.VISIBLE);
+        }else{
+            emailMsg.setVisibility(View.INVISIBLE);
+        }
+
+        if(!agreeCheckBox.isChecked()){
+            agreeMsg.setVisibility(View.VISIBLE);
+        }else{
+            agreeMsg.setVisibility(View.INVISIBLE);
+        }
+
+        if(passwordEditText.getText().toString().isEmpty()){
+            passwordMsg.setVisibility(View.VISIBLE);
+        }else{
+            passwordMsg.setVisibility(View.INVISIBLE);
+            if(passwordEditText.getText().toString().equals(cPasswordEditText.getText().toString())) {
+                matchPasswordMsg.setVisibility(View.INVISIBLE);
+            }else{
+                matchPasswordMsg.setVisibility(View.VISIBLE);
             }
-        };
-        queue.add(stringRequest);
+        }
+        if(passwordEditText.getText().toString().equals(cPasswordEditText.getText().toString()) &&
+                agreeCheckBox.isChecked() &&
+                !emailEditText.getText().toString().isEmpty() &&
+                !userNameEditText.getText().toString().isEmpty()
+            ){
+            RequestQueue queue = Volley.newRequestQueue(this);
+            String url = "https://mtd319-ed05.restdb.io/rest/credential?apikey=6357f014626b9c747864aeeb";
+            StringRequest stringRequest = new StringRequest(Request.Method.POST,url,
+                    response -> Toast.makeText(this,"Sign Up Success",Toast.LENGTH_SHORT).show(),
+                    error -> Toast.makeText(this,"Sign Up Error",Toast.LENGTH_SHORT).show()) {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Credential credential = new Credential(userNameEditText.getText().toString(),passwordEditText.getText().toString());
+                    Gson gson = new Gson();
+                    String jsonString = gson.toJson(credential);
+                    Map map = gson.fromJson(jsonString, Map.class);
+                    Log.d("getMap: ",map.toString());
+                    return map;
+                }
+            };
+            queue.add(stringRequest);
+            intent = new Intent(this,SignInActivity.class);
+            startActivity(intent);
+        }
     }
 }
